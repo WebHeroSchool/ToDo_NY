d94e8b17cdaf434e880ced9d6a45c739006c146d
NikolayYurevich <nik_755@mail.ru>
2022-01-19 23:48:04 +0700

    Lesson18 much done!

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,15 @@
 import React from 'react'; 
+import InputItem from '../InputItem/InputItem'
 import ItemList from '../ItemList/ItemList'
+import Footer from '../Footer/Footer'
+
 
 const App = () => (
 	<div>
 		<h1>Важные дела:</h1>
+		<InputItem />
 		<ItemList />
-	</div>
-)
+		<Footer />
+	</div>);
 
 export default App;
