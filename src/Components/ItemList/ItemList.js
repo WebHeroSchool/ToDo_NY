a02bee64de98af8a42e1d7d84d1341b4f5c0450b
NikolayYurevich <nik_755@mail.ru>
2022-01-20 00:23:01 +0700

    Lesson19 done!

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -1,10 +1,10 @@
 import React from 'react'; 
 import Item from '../Item/Item'
 
-const ItemList = () => (<ul>
-    <li><Item /></li>
-    <li><Item /></li>
-    <li><Item /></li>    
+const ItemList = ({ todoItems }) => (<ul>
+    <li><Item todoItem={todoItems.task1} /></li>
+    <li><Item todoItem={todoItems.task2} /></li>
+    <li><Item todoItem={todoItems.task3} /></li>    
     </ul>);
 
 export default ItemList;
